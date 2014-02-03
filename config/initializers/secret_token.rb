edb45ec0284dfd8483aca2deeb85d87dd80be223
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2014-02-03 22:04:21 +0300

    added .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xnotify::Application.config.secret_key_base = '084c09e429ef0d0a9c019dccde1e0a5950fb6ee6a58c38e3d4f1627d07f37832723cef7e40370daac3bdb0d0b0a092091fc5f7e1ca5ada826f5be6cefc9b545a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xnotify::Application.config.session_store :cookie_store, key: '_xnotify_session'
