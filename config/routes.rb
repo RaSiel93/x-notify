3710f8500a68afda3472c2ed52bb7f2780045473
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2014-02-15 21:41:16 +0300

    fix bug with form discounts partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Xnotify::Application.routes.draw do
   root to: 'items#index'
-  
+
   namespace :items do
     resources :advertisings, :events, :discounts
   end
 
   resources :items
   post '/items/:id/send', to: 'items#send_item_id', as: 'send_item'
-  
+
   resources :users
 
   get '/users/:id/send', to: 'users#show_send', as: 'send_user'
   post '/users/:id/send', to: 'users#send_message', as: 'send'
-  
+
   post '/api/reg', to: 'users#create', as: 'user_create'
   get '/api/items/:id(.:format)', to: 'items#show'
 
