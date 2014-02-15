09f275883cea246be68a862239ce2b319b6dbb8e
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2014-02-15 14:41:00 +0300

    set rote root (items_path)

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Xnotify::Application.routes.draw do
   end
 
   resources :items
-
+  post '/items/:id/send', :to => 'items#send_item_id', :as => 'send_item'
+  
+  root to: 'items#index'
+  
   # resources :users
 
   # root :to => 'items#index'
 
   # get '/users/:id/send', :to => 'users#show_send', :as => 'send_user'
-  post '/items/:id/send', :to => 'items#send_item_id', :as => 'send_item'
 	# post '/users/:id/send', :to => 'users#send_message', :as => 'send'
 
   # post '/api/reg', :to => 'users#create', :as => 'user_create'
